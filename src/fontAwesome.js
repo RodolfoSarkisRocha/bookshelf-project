376707fed276f1d105f97b814153c9db02986b02
Rodolfo Sarkis Rocha <rodolfosarkisrocha@pop-os.localdomain>
2020-06-16 00:03:01 -0300

    feature/home (home view created)

--- src/fontAwesome.js
+++ src/fontAwesome.js
@@ -1,4 +1,4 @@
-import {library} from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { faBars, faCalendar, faSearch, faArrowUp, faArrowDown, faBook, faSortAlphaUp, faUser, faPlus } from '@fortawesome/free-solid-svg-icons'
 
